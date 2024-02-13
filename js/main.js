21363f32df0f01727565e424014cdfb66055450b
dogstarr <159886411+dogstarr@users.noreply.github.com>
2024-02-13 17:29:40 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('#navigation', { delay: 300 });
+ScrollReveal().reveal('#logo', { delay: 300 });
+ScrollReveal().reveal('.promo-content', { delay: 300 });
+ScrollReveal().reveal('#photo', { delay: 300 });
+ScrollReveal().reveal('#tour', { delay: 300 });
+ScrollReveal().reveal('#video', { delay: 300 });
+ScrollReveal().reveal('#join', { delay: 300 });
+ScrollReveal().reveal('footer', { delay: 300 });
